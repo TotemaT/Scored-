b58f8a1577d5f6be231046e5871e3a68de268b0d
Matteo Taroli <taroli.matteo@gmail.com>
2016-07-14 14:58:43 +0200

    Add player

---
+++ app/src/main/java/be/matteotaroli/scored/Constants.java
@@ -0,0 +1,9 @@
+package be.matteotaroli.scored;
+
+/**
+ * Created by matt on 14/07/16.
+ */
+
+public interface Constants {
+    String defaultColour = "#FF0000";
+}
---
+++ app/src/main/java/be/matteotaroli/scored/pojos/Player.java
@@ -0,0 +1,36 @@
+package be.matteotaroli.scored.pojos;
+
+import be.matteotaroli.scored.Constants;
+
+public class Player {
+
+    private String name;
+    private int score;
+    private String color;
+
+    public Player(String name) {
+        this.name = name;
+        this.color = Constants.defaultColour;
+        this.score = 0;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void incrementScore() {
+        score = score++;
+    }
+}
