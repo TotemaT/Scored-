cb40f09c3fd5993bb5190bf39167e8e65cbf10a1
Matteo Taroli <taroli.matteo@gmail.com>
2016-07-21 22:42:07 +0200

    Add license in java files

--- app/src/main/java/be/matteotaroli/scored/Listeners/RecyclerRemoveItemListener.java
+++ app/src/main/java/be/matteotaroli/scored/Listeners/RecyclerRemoveItemListener.java
@@ -1,10 +1,24 @@
+/*
+Scored! allows to keep tracks of each player's score during a game.
+Copyright (C) 2016  Matteo Taroli
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package be.matteotaroli.scored.Listeners;
 
 import android.view.View;
-
-/**
- * Created by matt on 14/07/16.
- */
 
 public interface RecyclerRemoveItemListener {
     void onItemRemoved(View v, int position);
