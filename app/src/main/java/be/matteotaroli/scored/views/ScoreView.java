e05b456b57bbadb618eb602b0049b0a4f6a44d95
Matteo Taroli <taroli.matteo@gmail.com>
2016-12-07 21:36:15 +0100

    Add landscape layouts

--- app/src/main/java/be/matteotaroli/scored/views/ScoreView.java
+++ app/src/main/java/be/matteotaroli/scored/views/ScoreView.java
@@ -7,7 +7,6 @@ import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
